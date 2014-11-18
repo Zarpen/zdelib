f2d292fbc6de024495259bb816bfe16a3109f8b8
gcasado <gcasado@localhost.localdomain>
2014-11-18 13:18:52 +0100

    update examples css and use zdelib from master

--- css.js
+++ css.js
@@ -2,7 +2,7 @@ _().load(function(){
 	// prepare css
 	_().createCSS("decoration",{"attr":"type=text/css;"});
 	_().addRule([
-			["body","margin:0;padding:0;background:url('resources/img/bg.png') repeat scroll 0 0 transparent;font-family:Verdana;font-size:1em;"],
+			["body","margin:0;padding:0;background-color:#fafafa;font-family:Verdana;font-size:1em;"],
 			["input","display:inline-block;border:2px solid #ff5353;background-color:#97ff86;"],
 			["input:focus","background-color:white;"],
 			[".container","width:100%;height:100%"],
@@ -17,8 +17,8 @@ _().load(function(){
 			[".menu-list li a:hover","cursor:pointer;font-family:Arial;font-weight:bold;font-style:italic;font-size:1.2em;"],
 			[".stock-suggestions","border:1px solid black;margin:0;padding:0;border-collapse: collapse;background-color:white;font-family:Arial;font-weight:bold;"],
 			[".stock-suggestions tr:hover","background-color:#51ff76;border-bottom:1px dashed black;cursor:pointer;"],
-			[".quote-detail","margin-top:50px;border:1px dashed black;background-color:#fff59f;font-family:Times New Roman;font-size:0.8em;margin-left:auto;margin-right:auto;"],
-			[".quote-detail td","border-bottom:1px dashed black;"],
+			[".quote-detail","margin-top:50px;border:1px dashed black;background-color:#fff59f;font-family:Times New Roman;font-size:0.8em;margin-left:auto;margin-right:auto;table-layout: fixed;width:100%;word-wrap:break-word;"],
+			[".quote-detail td","border-bottom:1px dashed black;width:33%;"],
 			[".info-tip","padding:5px;-moz-border-radius: 15px;border-radius: 15px;border:2px solid black;font-family:Verdana;font-size:0.8em;font-style:italic;font-weight:bold;background-color:#ffa3a3;"],
 			[".calendar-box","text-align:center;background-color:#b5ddff;"],
 			[".calendar-header","border:1px solid black;border-collapse:collapse;"],
